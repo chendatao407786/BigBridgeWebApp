aa858c92234f48a39ff5ee76dd4b23a4353153e1
CHEN Datao <chendatao1126@gmail.com>
2019-01-27 19:46:03 +0100

    Update user.js

--- routers/api/user.js
+++ routers/api/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const config = require('../../config');
 const oracledb = require('oracledb');
 router.get('/', (req, res) => {
-    let query = 'SELECT u.id,u.name,u.DATE_NAISSANCE as BIRTHDAY, u.WEIGHT,u.SMOKING,u.SPORT,u.HEART_DISEASE as "HEART DISEASE",u.ASTHMA FROM USER_INFO u';
+    let query = 'SELECT u.name,u.DATE_NAISSANCE as BIRTHDAY,u.sex, u.WEIGHT,u.SMOKING,u.SPORT,u.HEART_DISEASE as "HEART DISEASE",u.ASTHMA FROM USER_INFO u';
     let parametres = {};
     oracledb.getConnection(
         config.connectionPool,
@@ -12,9 +12,9 @@ router.get('/', (req, res) => {
         }
     )
 })
-router.get('/:id', (req, res) => {
-    let query = "SELECT u.id,u.name,TO_CHAR(u.DATE_NAISSANCE,'dd/mm/yyyy') as BIRTHDAY, u.WEIGHT,u.SMOKING,u.SPORT,u.HEART_DISEASE as \"HEART DISEASE\",u.ASTHMA FROM USER_INFO u WHERE u.id = :id";
-    let parametres = { id: req.params.id };
+router.get('/:username', (req, res) => {
+    let query = "SELECT u.name,u.sex,TO_CHAR(u.DATE_NAISSANCE,'dd/mm/yyyy') as BIRTHDAY, u.WEIGHT,u.SMOKING,u.SPORT,u.HEART_DISEASE as \"HEART DISEASE\",u.ASTHMA FROM USER_INFO u WHERE u.username = :username";
+    let parametres = { username: req.params.username};
     oracledb.getConnection(
         config.connectionPool,
         function (err, connection) {
@@ -23,15 +23,53 @@ router.get('/:id', (req, res) => {
     )
 })
 router.post('/', (req, res) => {
-    let query = "INSERT into USER_INFO (id,name,date_naissance,weight,smoking,sport,heart_disease,asthma) VALUES (13121235,:name,TO_DATE( :birthday, 'DD-MM-YYYY' ),:weight,:smoking,:sport,:heart_disease,:asthma)";
+    console.log(req.body);
+    let query = "INSERT into USER_INFO (username,name,date_naissance,sex,weight,smoking,sport,heart_disease,asthma) VALUES (:username,:name,TO_DATE( :birthday, 'DD-MM-YYYY' ),:sex,:weight,:smoking,:sport,:heart_disease,:asthma)";
     let parametres = {
+        username:req.body.username,
         name:req.body.name,
         birthday:req.body.birthday,
-        weight:parseInt(req.body.weight),
+        sex:req.body.sex,
+        weight:req.body.weight,
         smoking:req.body.smoking,
         sport:req.body.sport,
         heart_disease:req.body.heart_disease,
         asthma:req.body.asthma
+    }
+    oracledb.getConnection(
+        config.connectionPool,
+        function(err,connection){
+            config.executionSqlQuery(err,connection,query,parametres,res,oracledb.OBJECT)
+        }
+    )
+})
+
+router.put('/:username',(req,res)=>{
+    console.log(req.body);
+    console.log(req.params.username);
+    
+    let query = "UPDATE USER_INFO "+ 
+                "SET " + 
+                "name=:name,"+
+                "sex=:sex,"+
+                "date_naissance=TO_DATE(:birthday,'DD-MM-YYYY'),"+
+                "weight=:weight,"+
+                "smoking=:smoking,"+
+                "sport=:sport,"+
+                "heart_disease=:heart_disease,"+
+                "asthma=:asthma "+
+                "where username=:username";
+                
+    let parametres={
+        username:req.body.username,
+        name:req.body.name,
+        birthday:req.body.birthday,
+        sex:req.body.sex,
+        weight:req.body.weight,
+        smoking:req.body.smoking,
+        sport:req.body.sport,
+        heart_disease:req.body.heart_disease,
+        asthma:req.body.asthma 
     }
     oracledb.getConnection(
         config.connectionPool,
