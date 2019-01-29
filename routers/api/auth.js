0e06bebfccd39a3a575b27c877846fd3616d5f00
CHEN Datao <chendatao1126@gmail.com>
2019-01-29 15:06:28 +0100

    Update auth.js

--- routers/api/auth.js
+++ routers/api/auth.js
@@ -24,11 +24,45 @@ router.post('/login',(req,res)=>{
         username:req.body.username,
         password:req.body.password
     }
+
     oracledb.getConnection(
         config.connectionPool,
         function(err,connection){
-            config.executionSqlQuery(err,connection,query,parametres,res,oracledb.OBJECT)
+            if(err){
+                console.error(err.message);
+                return;
+            }
+            connection.execute(
+                query,
+                parametres,
+                {
+                    outFormat: oracledb.OBJECT,
+                    maxRows: 10,
+                    autoCommit: true
+                },
+                function(err,result){
+                    if(err){
+                        console.error(err.message);
+                        config.doRelease(connection);
+                        return;
+                    }else{
+                        if(result.rows.length>0){
+                            res.json(result.rows);
+                        }else{
+                            res.status(401).send("username or password incorrect");
+                        }
+                        config.doRelease(connection);
+                    }
+                }
+            )
         }
+
     )
+    // oracledb.getConnection(
+    //     config.connectionPool,
+    //     function(err,connection){
+    //         config.executionSqlQuery(err,connection,query,parametres,res,oracledb.OBJECT)
+    //     }
+    // )
 })
 module.exports = router;
