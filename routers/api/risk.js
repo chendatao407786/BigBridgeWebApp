84e79cc876ebe0eab12b5c1455fe3e0ecd4b8b50
CHEN Datao <chendatao1126@gmail.com>
2019-03-31 14:28:36 +0800

    Update risk.js

--- routers/api/risk.js
+++ routers/api/risk.js
@@ -7,18 +7,21 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     let data = req.query
-    var url = "http://127.0.0.1:4316/has-risk?bmi=" + data.bmi 
+    console.log(req.query);
+    
+    var url = "http://127.0.0.1:8528/has-risk?bmi=" + data.bmi 
             + "&station=" + data.station 
             + "&postalcode=" + data.postalcode 
             + "&drinking=" + data.drinking 
             + "&smoking=" + data.smoking 
             + "&gender=" + data.gender 
-            + "&height=" + data.height 
+            + "&height=" + data.height
             + "&weight=" + data.weight 
             + "&age=" + data.age;
     request(url, (error, response, body) => {
         if (error) {
-            res.statusCode(500);
+            console.log(error);
+            res.status(500);
         } else {
             let json = JSON.parse(response.body);
             let oui = json.person[0].POui;
